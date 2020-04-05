5e2f207b17050a5fa3c022b1205c5d6d5e49477d
Álvaro Fernández Rojas <noltari@gmail.com>
2020-04-05 12:52:24 +0200

    u-boot-jffs2: create empty bin directory
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- u-boot-jffs2.sh
+++ u-boot-jffs2.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+create_bin_dir=1
+
 usage() {
-	echo "Usage:"
-	echo "$(basename $0) -e <erase block> -i <input file> -o <output file>"
+	echo "Usage: $(basename $0)"
+	echo "\t[-d]: Disable bin dir creation"
+	echo "\t-e <erase block>: Erase block size (e.g. 128k)"
+	echo "\t-i <input file>: Input file (e.g. cferam.000)"
+	echo "\t-o <output file>: Output file (e.g. cferam.jffs2)"
 }
 
 jffs2_create() {
@@ -13,6 +18,10 @@ jffs2_create() {
 			erase_block=$(echo "$(echo $erase_block | tr -d k) * 1024" | bc -l )
 			;;
 	esac
+
+	if [ $create_bin_dir -eq 1 ]; then
+		mkdir -p $root_dir/bin
+	fi
 
 	cp $input_file $root_dir
 
@@ -29,7 +38,7 @@ jffs2_create() {
 }
 
 main() {
-	while getopts ":e:hi:o:" opt; do
+	while getopts ":de:hi:o:" opt; do
 		case "${opt}" in
 			'e')
 				erase_block=$OPTARG
@@ -42,6 +51,9 @@ main() {
 				;;
 			'o')
 				output_file=$OPTARG
+				;;
+			'd')
+				create_bin_dir=0
 				;;
 		esac
 	done
